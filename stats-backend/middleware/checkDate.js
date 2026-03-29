717e9e9d4f803a6b2a603ebd4940cfd0cedecc84
Randude14 <Rander510@aol.com>
2026-03-28 23:37:23 -0500

    Clean up middleware.

--- stats-backend/middleware/checkDate.js
+++ stats-backend/middleware/checkDate.js
@@ -1,19 +1,25 @@
 function checkDate(dateField) {
 
     if(!dateField || dateField.length === 0) {
-        return null;
+        throw new Error("dateField required.");
     }
 
-    return async (req, res, next) => {
+    return (req, res, next) => {
         const date = req.body[dateField];
 
-        if(!date || date.length === 0) {
+        if(date === undefined || date === null) {
             return res.status(400).json({ error: `${dateField} not provided` })
         }
 
         // Date is expected to be in format YYYY-MM-DD
         if( !/^\d{4}-\d{2}-\d{2}$/.test(date) ) {
             return res.status(400).json({ error: `${dateField} is not in the correct format (YYYY-MM-DD)` })
+        }
+
+        // Validate actual date
+        const dateObj = new Date(value);
+        if (isNaN(dateObj.getTime())) {
+            return res.status(400).json({ error: `${dateField} is not a valid date` });
         }
 
         next();
--- stats-backend/middleware/checkNotNegative.js
+++ stats-backend/middleware/checkNotNegative.js
@@ -1,27 +1,29 @@
 function checkNotNegative(numberField) {
 
     if(!numberField || numberField.length === 0) {
-        return async(req, res, next) => next();
+        throw new Error("numberField required.");
     }
 
-    return async (req, res, next) => {
+    return (req, res, next) => {
         let number = req.body[numberField];
 
-        if(number === undefined || number === null) {
+        if(value === undefined || value === null) {
             return res.status(400).json({ error: `${numberField} not provided` })
         }
 
-        if(typeof number === 'string') {
-            number = +number
+        if(typeof value === 'string') {
+            value = value(value)
         }
 
-        if(typeof number != 'number') {
-            return res.status(400).json({ error: `${numberField} is not a number or string` })
+        if (!value.isFinite(value)) {
+            return res.status(400).json({ error: `${field} must be a valid value` });
         }
 
-        if(number < 0) {
+        if(value < 0) {
             return res.status(400).json({ error: `${numberField} is not >= 0` })
         }
+
+        req.body[numberField] = value;
 
         next();
     };
