ddecba35646aa4fcaea9b5071ae1b0b9af599ebc
Randude14 <Rander510@aol.com>
2026-04-08 17:20:42 -0500

    Add basic page functionality for the info table to go between pages of multiple results

--- stats-frontend/src/components/InfoTable.tsx
+++ stats-frontend/src/components/InfoTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ReactElement } from "react"
+import { useEffect, useState, useRef, type ReactElement } from "react"
 import { useAuth } from "../context/useAuth";
 import { fetchWithAuth, fetchWithNoAuth, HttpMethod } from "../util/serverRequests";
 import './InfoTable.css'
@@ -8,12 +8,14 @@ type RowObject = Record<string, unknown>;
 interface ColumnInfoSettings {
     auth: boolean, // whether to use user token
     endpoint: string, // API endpoint to get rows from
+    httpMethod?: string, // HTTP method to use, GET if not passed
     rowFields: string[], // Array of fields to pull from rows
+    pageSize?: number,  // Sets the page limit, if set to 0 or less, disables the prev/next buttons
     columnName: (field: string) => string, // Function passed to get field name from column
     rowFieldBuilder: (field: string, data: RowObject) => ReactElement, // Function passed to get elements based on field and data
 }
 
-function buildColumnHeaders({rowFields, columnName}: ColumnInfoSettings) {
+function buildColumnHeaders(rowFields: string[], columnName: (field: string) => string) {
 
     const columnNames: string[] = rowFields.map(col => columnName(col));
 
@@ -26,7 +28,7 @@ function buildColumnHeaders({rowFields, columnName}: ColumnInfoSettings) {
         </>
 }
 
-function buildRows<T extends RowObject>({rowFields, rowFieldBuilder}: ColumnInfoSettings, rows: T[]) {
+function buildRows<T extends RowObject>(rowFields: string[], rowFieldBuilder: (field: string, data: RowObject) => ReactElement, rows: T[]) {
 
     return <>
         {
@@ -40,32 +42,33 @@ function buildRows<T extends RowObject>({rowFields, rowFieldBuilder}: ColumnInfo
 }
 
 // Generics type allows table to work with an inhertied object type
-export function InfoTable<T extends RowObject>(settings: ColumnInfoSettings) {
+export function InfoTable<T extends RowObject>({auth, endpoint, httpMethod, pageSize, rowFields, rowFieldBuilder, columnName}: ColumnInfoSettings) {
 
     const { token } = useAuth();
-
     const [rows, setRows] = useState<T[]>([]);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
-
-    if(!settings.endpoint || !settings.rowFields || !settings.columnName || !settings.rowFieldBuilder) {
-        return <strong>Invalid settings.</strong>
-    }
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         const fetchRows = async () => {
             setLoading(true);
             setErrorMessage(null);
 
-            if (settings.auth && !token) {
+            if(!endpoint) {
+                return;
+            }
+
+            if (auth && !token) {
                 setErrorMessage("Please log in first.");
                 setRows([]);
                 setLoading(false);
                 return;
             }
 
-            const endpointFetch = settings.auth ? fetchWithAuth : fetchWithNoAuth;
-            const res = await endpointFetch(settings.endpoint, HttpMethod.GET);
+            const endpointFetch = auth ? fetchWithAuth : fetchWithNoAuth;
+            // for now assume endpoint already has the correct format before adding, this should probably get fixed later
+            const res = await endpointFetch(pageSize ? `${endpoint}&page=${page}&pageSize=${pageSize}` : endpoint, httpMethod || HttpMethod.GET);
 
             let data: unknown = null;
             try {
@@ -102,7 +105,17 @@ export function InfoTable<T extends RowObject>(settings: ColumnInfoSettings) {
         };
 
         fetchRows();
-    }, [settings.auth, settings.endpoint, token]);
+    }, [auth, endpoint, httpMethod, token, page, pageSize]);
+
+    // safely return nothing if endpoint is null
+    // some pages have search fields
+    if(!endpoint) {
+        return <></>
+    }
+
+    if(!rowFields || !columnName || !rowFieldBuilder) {
+        return <strong>Invalid settings.</strong>
+    }
 
     if(loading) {
         return <strong>Loading...</strong>
@@ -112,14 +125,27 @@ export function InfoTable<T extends RowObject>(settings: ColumnInfoSettings) {
         return <strong>{errorMessage}</strong>
     }
 
-    const columns: number = settings.rowFields.length;
+    const columns: number = rowFields.length;
+    const shouldDisablePrev = pageSize <= 0 || page === 1;
+    const shouldDisableNext = pageSize <= 0 || rows.length < pageSize;
+
+    const onPrevClickHandler = () => {
+        setPage(Math.max(page-1, 1));
+    }
+    const onNextClickHandler = () => {
+        setPage(page+1);
+    }
 
     return <>
         <div className="info-table" style={{ 
                 gridTemplateColumns: `repeat(${columns}, 1fr)`
             }}>
-            {buildColumnHeaders(settings)}
-            {buildRows(settings, rows)}
+            {buildColumnHeaders(rowFields, columnName)}
+            {buildRows(rowFields, rowFieldBuilder, rows.filter( (e, i) => i != rows.length-1))}
+            {pageSize > 0 && <div>
+                <button disabled={shouldDisablePrev} onClick={onPrevClickHandler}>Prev</button>
+                <button disabled={shouldDisableNext} onClick={onNextClickHandler}>Next</button>
+            </div>}
         </div>
     </>
 }
--- stats-frontend/src/pages/AllGamesScreen.tsx
+++ stats-frontend/src/pages/AllGamesScreen.tsx
@@ -1,5 +1,6 @@
-import { type ReactElement } from "react"
+import { useRef, useState, type ReactElement } from "react"
 import { InfoTable } from "../components/InfoTable";
+import { HttpMethod } from "../util/serverRequests";
 
 type GameDataRow = {
     title: string,
@@ -28,16 +29,44 @@ const columnNames = (field: string): string => {
 const rowFieldBuilder = (field: string, data: GameDataRow): ReactElement => {
     let labelString: string = 'Error';
     switch (field) {
-        case Fields.GAME_TITLE: 
-        case Fields.PUBLISHER: 
-        case Fields.DEVELOPER: labelString = data[field]; break;
-        case Fields.GAME_RELEASE: labelString = new Date(data[field]).toLocaleDateString(); break;
+        case Fields.GAME_TITLE: labelString = data[field]; break;
+        case Fields.PUBLISHER: labelString = data['publishers'].length > 0 ? data['publishers'][0] : 'N/A'; break;
+        case Fields.DEVELOPER: labelString = data['developers'].length > 0 ? data['developers'][0] : 'N/A'; break;
+        case Fields.GAME_RELEASE: labelString = new Date(data['release_date']).toLocaleDateString(); break;
     }
     return <label key={field}>{labelString}</label>;
 }
 
+const getSearchPoint = (text: string) => {
+    if(!text) {
+        return null;
+    }
+    return `games/external/search?query=${text}`;
+}
+
 export function AllGamesScreen() {
     const rowFields: string[] = [Fields.GAME_TITLE, Fields.DEVELOPER, Fields.PUBLISHER, Fields.GAME_RELEASE];
+    const gameSearchText = useRef<HTMLInputElement | null>(null);
+    const buttonSearchRef = useRef<HTMLButtonElement | null>(null);
+    const [searchPoint, setSearchPoint] = useState<string | null>(null);
 
-    return <InfoTable<GameDataRow> auth={true} endpoint="games" rowFields={rowFields} rowFieldBuilder={rowFieldBuilder} columnName={columnNames} />
+    const onTextChangeHandler = () => {
+        if(buttonSearchRef.current && gameSearchText.current) {
+            buttonSearchRef.current.disabled = gameSearchText.current.value.length === 0;
+        }
+    }
+
+    const onClickHandler = () => {
+        const searchEndpoint: string = getSearchPoint(gameSearchText.current ? gameSearchText.current.value : '');
+        setSearchPoint(searchEndpoint);
+    }
+
+    return <>
+        <div>
+                <input type="search" ref={gameSearchText} onChange={onTextChangeHandler} placeholder="Enter game name here"></input>
+                <button onClick={onClickHandler} ref={buttonSearchRef}>Search</button>
+        </div>
+        <InfoTable<GameDataRow> key={searchPoint} auth={false} pageSize={30} endpoint={searchPoint} httpMethod={HttpMethod.POST} rowFields={rowFields} rowFieldBuilder={rowFieldBuilder} columnName={columnNames} />
+    </>
+
 }
